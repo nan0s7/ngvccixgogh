77f46fe7aee1b29e3957217b8e65bf6bc3ad93fa
nan0s7 <scottwalters97@gmail.com>
2017-10-27 13:10:35 +1100

    Updated to use new template method

--- nlinker.sh
+++ nlinker.sh
@@ -1,62 +1,25 @@
 #!/bin/bash
 
 # Variable initialisation
-out=""
-tmp=""
-colours=""
-end="8"
-name="$2"
 picture="$1"
-declare -a col_arr=()
+colours=""
+name="$2"
 
 function finish {
     unset colours
-    unset piccture
+    unset picture
     unset name
-    unset out
-    unset tmp
-    unset col_arr
-    unset end
-    unset start
 }
 trap finish EXIT
 
 echo
 echo "--------------------> Getting colours..."
 
-# Will need to adjust this to allow for arguments to be sent to extract.py
-touch output.txt
-python3 ../gvcci/extract.py $picture >output.txt
-out=`cat output.txt`
-
-# Start organising the colours so ngoghbase.sh can understand them
-tmp="${out#*Background*}"
-tmp="${tmp:0:8}"
-col_arr+=( $tmp )
-
-tmp=""
-tmp="${out#*Foreground*}"
-tmp="${tmp:0:8}"
-col_arr+=( $tmp )
-
-for j in `seq 0 1`; do
-    if [ "$j" -eq "0" ]; then
-        out="${out#*Normal*}"
-    else
-        out="${out#*Bright*}"
-    fi
-    for i in `seq 0 $[ 7 ]`; do
-        start=$[ $i * 8 ]
-        tmp=""
-        tmp="${out:$start:$end}"
-        col_arr+=( $tmp )
-    done
-done
-
-arr_len="${#col_arr[@]}"
-for i in `seq 0 $arr_len`; do
-    colours="$colours""${col_arr[$i]}"
-done
+cd ../gvcci
+python3 extract.py $picture --template templates/nospace.txt
+cp gvcci.txt ../ngvccixgogh/
+cd ../ngvccixgogh
+colours=`cat gvcci.txt`
 
 echo "--------------------> Passing colours into applier script"
 echo
@@ -65,6 +28,3 @@ echo "Name: "$name
 echo
 
 nohup ./ngoghbase.sh $colours $name >/dev/null 2>&1 &
-
-echo "--------------------> Cleaning up..."
-rm output.txt
