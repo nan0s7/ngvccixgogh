5721aff0d1df8ebede1716060990e3d40b2e3d05
nan0s7 <scottwalters97@gmail.com>
2017-12-28 17:45:24 +1100

    Updated to latest gvcci version (untested, though)

--- nlinker.sh
+++ nlinker.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 # Variable initialisation
-picture="$1"
+pic_path="$1"
+ext_len="${pic_path#*.*}"
+ext_len="${#ext_len}"
+ext_len="$[ $ext_len - $ext_len - $ext_len - 1 ]"
+path_len="${pic_path%*/*}"
+path_len="${#path_len}"
+path_len="$[ $path_len + 1 ]"
+picture="${pic_path:$path_len:$ext_len}"
 colours=""
 name="$2"
+user="$(whoami)"
 
 function finish {
     unset colours
     unset picture
     unset name
+    unset user
+    unset path_len
+    unset ext_len
+    unset pic_path
 }
 trap finish EXIT
 
@@ -16,9 +28,11 @@ echo
 echo "--------------------> Getting colours..."
 
 cd ../gvcci
-python3 extract.py $picture --template templates/nospace.txt
-cp gvcci.txt ../ngvccixgogh/
+#python3 extract.py $picture --template templates/nospace.txt
+./gvcci.sh $pic_path --template templates/nospace.txt
+#cp gvcci.txt ../ngvccixgogh/
 cd ../ngvccixgogh
+cp "/home/$user/.gvcci/themes/$picture/nospace.txt" "gvcci.txt"
 colours=`cat gvcci.txt`
 
 echo "--------------------> Passing colours into applier script"
